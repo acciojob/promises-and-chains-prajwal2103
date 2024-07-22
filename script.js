80c6006cf14958ea0307566a1a1113b4ef7347b6
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2024-07-22 20:55:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+let form=document.querySelector('form');
+form.addEventListener('submit',(event)=>{
+  let age=document.getElementById('age');
+  let name=document.getElementById('name');
+  let prom=new Promise((res,rej)=>{
+  // console.log(age);
+  // console.log(name);
+    if(age>=18){
+      setTimeout(()=>{
+        res(`Welcome, ${name}. You can vote.`)
+      },4000)
+     
+    }
+    else{
+      setTimeout(()=>{
+        rej(`Oh sorry ${name}. You aren't old enough.`)
+      },4000)
+    }
+   
+  })
+  prom.then(data=>{
+   alert(data)
+  }).catch(data=>{
+    alert(data)
+  })
+
+})
